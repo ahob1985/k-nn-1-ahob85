09a3e2d8f1c0d2c0181d6d37f00b8c18a725b024
ahob85 <aaron@ahob85.com>
2021-07-02 20:59:22 +0000

    started project

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-// Author:
+// Author: Aaron Hobson
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
+let textP2;
+let buttonDiv;
+let upButton;
+let downButton;
+let leftButton;
+let rightButton;
 
 // Global ML variables
-//let featureExtractor;
+let featureExtractor;
+let imgFeatures;
+let knnClassifier;
+let video;
+let isModelReady;
+let ups;
+let downs;
+let lefts;
+let rights;
 
 function setup() {
-
+  // build canvas and text ui 
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model loading, please wait...");
+  textP.parent(textDiv);
+  textP2 = createP("[Training data here.]");
+  textP2.parent(textDiv);
+  // build buttons 
+  buildButtons();
+  // initialize ups, downs, lefts, rights 
+  ups = 0;
+  downs = 0;
+  lefts = 0;
+  rights = 0;
+  // load video 
+  video = createCapture(VIDEO, videoReady);
 }
 
 function draw() {
